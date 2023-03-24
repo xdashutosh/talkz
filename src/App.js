ba3ed13578868e484438c7007c9c930f85993f8e
ashutosh chaudhary <ashuchaudhary6969@gmail.com>
2023-03-24 11:28:59 +0530

    setted up firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,45 +1,69 @@
-import React from "react";
+import {React,useState,useEffect} from "react";
 import {Box,Container,Input,VStack,Button,HStack} from "@chakra-ui/react";
 import Message from "./components/Message";
+import { GoogleAuthProvider,signOut,signInWithPopup,getAuth,onAuthStateChanged} from "firebase/auth"
+import {app} from "./Firebase"
+const auth =getAuth(app);
+
+const loginhandler =()=>{
+  const provider=new GoogleAuthProvider();
+  signInWithPopup(auth,provider);
+}
+ const signouthandler=()=>{
+signOut(auth);  
+}
+
 function App() {
+const [useravail,setuseravail]=useState(false);
+useEffect(() => {
+onAuthStateChanged(auth,(data)=>{
+  setuseravail(data);
+})
+})
   return (
     <Box bg={"red.50"}>
-      <Container h={"100vh"} bg={"white"}>
-        <VStack h="full" paddingY={4}>
-          <Button className="btn" w={"full"} colorScheme={"red"}>
-            Logout
-          </Button>
+          {
+            useravail?( <Container h={"100vh"} bg={"white"}>
+            <VStack h="full" paddingY={4}>
+              <Button onClick={signouthandler} w={"full"} colorScheme={"red"} >
+                Logout
+              </Button>
+    
+          <VStack  h={"full"} w="full" overflowY="auto">
+            <Message text="hey ashu" user="other"/>
+            <Message text="hey abhi " user="me" />
+            <Message text="hey abhi " user="me" />
+            <Message text="hey ashu" user="other"/>
+            <Message text="hey ashu" user="other"/>
+            <Message text="hey abhi " user="me" />
+            <Message text="hey abhi " user="me" />
+            <Message text="hey ashu" user="other"/>
+            <Message text="hey abhi " user="me" />
+            <Message text="hey ashu" user="other"/>
+            <Message text="hey abhi " user="me" />
+            <Message text="hey abhi " user="me" />
+            <Message text="hey ashu" user="other"/>
+            <Message text="hey ashu" user="other"/>
+            <Message text="hey ashu" user="other"/>
+            <Message text="hey ashu" user="other"/>
+            <Message text="hey abhi " user="me" />
+            <Message text="hey abhi " user="me" />
+          </VStack>
+    
+          <form style={{width: "100%"}}>
+            <HStack>
+            <Input placeholder="Enter a message..."/>
+            <Button type="submit" colorScheme={"purple"}>Send</Button>
+            </HStack>
+          </form>
+    
+            </VStack>
+          </Container>):<VStack h={"100vh"} w={"full"} justifyContent={"center"}> 
+              <Button onClick={loginhandler} colorScheme={"purple"}>signIn With Google</Button>
+              
+          </VStack>
+          }
 
-      <VStack  h={"full"} w="full" overflowY="auto">
-        <Message text="hey ashu" user="other"/>
-        <Message text="hey abhi " user="me" />
-        <Message text="hey abhi " user="me" />
-        <Message text="hey ashu" user="other"/>
-        <Message text="hey ashu" user="other"/>
-        <Message text="hey abhi " user="me" />
-        <Message text="hey abhi " user="me" />
-        <Message text="hey ashu" user="other"/>
-        <Message text="hey abhi " user="me" />
-        <Message text="hey ashu" user="other"/>
-        <Message text="hey abhi " user="me" />
-        <Message text="hey abhi " user="me" />
-        <Message text="hey ashu" user="other"/>
-        <Message text="hey ashu" user="other"/>
-        <Message text="hey ashu" user="other"/>
-        <Message text="hey ashu" user="other"/>
-        <Message text="hey abhi " user="me" />
-        <Message text="hey abhi " user="me" />
-      </VStack>
-
-      <form style={{width: "100%"}}>
-        <HStack>
-        <Input placeholder="Enter a message..."/>
-        <Button type="submit" colorScheme={"purple"}>Send</Button>
-        </HStack>
-      </form>
-
-        </VStack>
-      </Container>
     </Box>
   );
 }
---
+++ src/Firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiaKiGCeMZnIVRf3x_iAbYLGDA-c2qSSk",
+  authDomain: "talkz-77d3c.firebaseapp.com",
+  projectId: "talkz-77d3c",
+  storageBucket: "talkz-77d3c.appspot.com",
+  messagingSenderId: "1081698530268",
+  appId: "1:1081698530268:web:dce0beee3bf8c925f5d519"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
